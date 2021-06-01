cf6996f02cdfd37ee109438ad822a4300a31ab0e
Deep Suhag / Deepmanyu Suhag <Deepmanyu.suhag@gmail.com>
2021-06-01 15:10:56 -0400

    Add files via upload

--- helpers.c
+++ helpers.c
@@ -11,7 +11,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width; j++)
+        for (int j = 0; j < width; j++) 
         {
             RGBTRIPLE pixel = image[i][j];
             int average = round((float) (pixel.rgbtRed + pixel.rgbtGreen + pixel.rgbtBlue)/3.0);
